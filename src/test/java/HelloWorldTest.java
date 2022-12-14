c1deb289f74c9d47892c313f50370cd954ad998f
Kaerulius2 <kaerulius2@yandex.ru>
2022-12-14 20:05:56 +0300

    Update HelloWorldTest.java
    Ex5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,14 +1,25 @@
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HelloWorldTest {
 
     @Test
-    public void getTextTest(){
-      Response response = RestAssured
-              .get("https://playground.learnqa.ru/api/get_text")
-              .andReturn();
-      response.prettyPrint();
+    public void testRestAssuredEx5(){
+
+        JsonPath response = RestAssured
+              .get("https://playground.learnqa.ru/api/get_json_homework")
+              .jsonPath();
+
+        List messages = response.getList("messages.message");
+        System.out.println(messages.get(1));
+
     }
 }
