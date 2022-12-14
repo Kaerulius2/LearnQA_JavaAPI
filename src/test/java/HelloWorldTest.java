48ac1bbd5256207ffd23907d35092283e806078c
Kaerulius2 <kaerulius2@yandex.ru>
2022-12-14 20:33:57 +0300

    Ex7

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import java.lang.reflect.Array;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,15 +14,22 @@ public class HelloWorldTest {
 
 
     @Test
-    public void testRestAssuredEx5(){
+    public void testRestAssuredEx7(){
+        int statusCode;
+        String path = "https://playground.learnqa.ru/api/long_redirect";
+        do {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .get(path)
+                    .andReturn();
+            statusCode = response.getStatusCode();
+            path = response.getHeader("location");
+            System.out.println(path);
+        } while (statusCode!=200);
 
-        Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-              .get("https://playground.learnqa.ru/api/long_redirect")
-              .andReturn();
 
-        System.out.println(response.getHeader("location"));
+
     }
 }
