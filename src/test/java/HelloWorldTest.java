ffe59ed5ce41a2e477f835b8ccdacf451f2f367d
Kaerulius2 <kaerulius2@yandex.ru>
2022-12-14 21:05:10 +0300

    Ex8

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,5 @@
 import io.restassured.path.json.JsonPath;
+import org.apache.http.util.Asserts;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -14,22 +15,41 @@ public class HelloWorldTest {
 
 
     @Test
-    public void testRestAssuredEx7(){
-        int statusCode;
-        String path = "https://playground.learnqa.ru/api/long_redirect";
-        do {
-            Response response = RestAssured
+    public void testRestAssuredEx7() throws InterruptedException {
+
+
+            JsonPath response = RestAssured
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
+
+            String token = response.get("token");
+            int seconds = response.get("seconds");
+
+
+            JsonPath response2 = RestAssured
                     .given()
-                    .redirects()
-                    .follow(false)
-                    .get(path)
-                    .andReturn();
-            statusCode = response.getStatusCode();
-            path = response.getHeader("location");
-            System.out.println(path);
-        } while (statusCode!=200);
+                    .queryParam("token",token)
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
 
+            String status = response2.get("status");
 
+            assert status.equals("Job is NOT ready");
 
+            Thread.sleep(seconds*1000);
+
+            JsonPath response3 = RestAssured
+                    .given()
+                    .queryParam("token",token)
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
+
+            status = response3.get("status");
+
+            assert status.equals("Job is ready");
+
+            String result = response3.get("result");
+
+            assert !result.equals(null);
     }
 }
